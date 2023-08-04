2160ee4d8b8a30d9cf3b80c0a00edb5f39b22307
gitdevstar <loganph.work@gmail.com>
2023-08-04 09:16:15 -0400

    change header profile

--- resources/components/layouts/header/fixed-header.vue
+++ resources/components/layouts/header/fixed-header.vue
@@ -19,129 +19,11 @@
                             <i class="fa fa-arrows-alt"></i>
                         </a>
                     </div>
-                    <b-dropdown class="notifications-menu bell_bg" right link>
-                        <span slot="text">
-                            <i class="fa fa-bell-o noti-icon"></i>
-                            <div class="notifications_badge_top">
-                                <span class="badge badge-danger text-white">4
-                                </span>
-                            </div>
-                        </span>
-                        <b-dropdown-item class="dropdownheader socio-tabs1" exact>
-                            <b-tabs>
-                                <b-tab title="Notifications" class="tabs_cont" active>
-                                    <b-dropdown-item exact>
-                                        <div class="row">
-                                            <div class="col-2 mt-2 ml-2">
-                                                <img class="rounded-circle" src="">
-                                            </div>
-                                            <div class="col-9 mt-2">
-                                                <p> &nbsp;Lorem ipsum dolor sit amet, elit.
-                                                    <br>
-                                                    <small class="ml-1 text-info">Today</small>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </b-dropdown-item>
-                                    <b-dropdown-item exact>
-                                        <div class="row">
-                                            <div class="col-2 mt-2 ml-2">
-                                                <img class="rounded-circle" src="">
-                                            </div>
-                                            <div class="col-9 mt-2">
-                                                <p> &nbsp;Lorem ipsum dolor sit amet, elit.
-                                                    <br>
-                                                    <small class="ml-1 text-muted">week ago</small>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </b-dropdown-item>
-                                    <b-dropdown-item exact>
-                                        <div class="row">
-                                            <div class="col-2 mt-2 ml-2">
-                                                <img class="rounded-circle" src="">
-                                            </div>
-                                            <div class="col-9 mt-2">
-                                                <p> &nbsp;Lorem ipsum dolor sit amet, elit.
-                                                    <br>
-                                                    <small class="ml-1 text-muted">month ago</small>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </b-dropdown-item>
-                                    <b-dropdown-item exact class="mt-2 notifications_data">
-                                        <div class="row">
-                                            <div class="col-2 mt-2 ml-2">
-                                                <img class="rounded-circle" src="">
-                                            </div>
-                                            <div class="col-9 mt-2">
-                                                <p> &nbsp;Lorem ipsum dolor sit amet, elit.
-                                                    <br>
-                                                    <small class="ml-1 text-muted">2 months ago</small>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </b-dropdown-item>
-                                    <b-dropdown-item class="dropdown-footer " exact>
-                                        <div>
-                                            <strong>View all</strong>
-                                        </div>
-                                    </b-dropdown-item>
-                                </b-tab>
-                                <b-tab title="Events" class="event_date">
-                                    <b-dropdown-item exact class="notifications_data">
-                                        <div class="noti_item">
-                                            <div class="row">
-                                                <div class="col-2   ml-3">
-                                                    <i class="fa fa-calendar text-info noti_msg"></i>
-                                                </div>
-                                                <div class="col-9 mt-1">
-                                                    <span>New Lorem Event from john.</span>
-                                                    <br>
-                                                    <span class="text-muted">Apr 29th 2017</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </b-dropdown-item>
-                                    <b-dropdown-item exact class="notifications_data">
-                                        <div class="row">
-                                            <div class="col-2 ml-3">
-                                                <i class="fa fa-calendar text-info noti_msg"></i>
-                                            </div>
-                                            <div class="col-9 mt-1">
-                                                <span>New Lorem Event from john.</span>
-                                                <br>
-                                                <span class="text-muted">Apr 29th 2017</span>
-                                            </div>
-                                        </div>
-                                    </b-dropdown-item>
-                                    <b-dropdown-item exact class="notifications_data">
-                                        <div class="row">
-                                            <div class="col-2 ml-3 mb-2">
-                                                <i class="fa fa-calendar text-info noti_msg"></i>
-                                            </div>
-                                            <div class="col-9 mt-1">
-                                                <span>New Lorem Event from john.</span>
-                                                <br>
-                                                <span class="text-muted">Apr 29th 2017</span>
-                                            </div>
-                                        </div>
-                                    </b-dropdown-item>
-                                    <b-dropdown-item class="dropdown-footer">
-                                        <div>
-                                            <strong>View all</strong>
-                                        </div>
-                                    </b-dropdown-item>
-                                </b-tab>
-                                <b-tab title="Updates" disabled>
-                                </b-tab>
-                            </b-tabs>
-                        </b-dropdown-item>
-                    </b-dropdown>
+
 
                     <!--rightside toggle-->
 
-                    <div class="dropdown hidden-xs-down btn-group " id="right-side" @click="toggle_right">
+                    <!-- <div class="dropdown hidden-xs-down btn-group " id="right-side" @click="toggle_right">
                         <a href="javascript:void(0)">
                             <i class="fa fa-envelope noti-icon"></i>
                             <div class="notifications_badge_top">
@@ -149,19 +31,19 @@
                                 </span>
                             </div>
                         </a>
-                    </div>
+                    </div> -->
 
 
                     <!--rightside toggle-->
 
                     <!-- User Account: style can be found in dropdown-->
-                    <b-dropdown class="user user-menu bell_bg user_btn" right link>
-                        <span slot="text">
+                    <b-dropdown class="user user-menu bell_bg user_btn position-static" right link>
+                        <span slot="button-content">
                             <img :src="this.$store.state.user.picture" class="rounded-circle" alt="User Image">
                             <!-- User name-->
                             <p class="user_name_max">{{name}}</p>
                         </span>
-                        <b-dropdown-item exact class="dropdown_content">
+                        <!-- <b-dropdown-item exact class="dropdown_content">
                             <router-link to="/user_profile" exact class="drpodowtext">
                                 <i class="fa fa-user-o"></i> Profile
                             </router-link>
@@ -175,7 +57,7 @@
                             <router-link to="/lockscreen" exact class="drpodowtext">
                                 <i class="fa fa-lock"></i> Lock
                             </router-link>
-                        </b-dropdown-item>
+                        </b-dropdown-item> -->
                         <b-dropdown-item exact class="dropdown_content">
                             <router-link to="/" class="drpodowtext">
                                 <div v-on:click="logout"><i class="fa fa-sign-out"></i> Logout</div>
