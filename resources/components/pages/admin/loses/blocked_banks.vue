6466aca9b73aff8bd68bb624129edcfa1ce5d8a1
gitdevstar <loganph.work@gmail.com>
2023-08-04 16:33:29 -0400

    change blocked banks

--- resources/components/pages/admin/loses/blocked_banks.vue
+++ resources/components/pages/admin/loses/blocked_banks.vue
@@ -3,15 +3,29 @@
         <div class="col-lg-12">
             <b-card header="Blocked Banks Table" header-tag="h4" class="bg-primary-card">
                 <div class="table-responsive">
-                    <datatable title="Blocked Banks" :rows="tableData" :columns="columndata" :columnDefs="columnDefs" url="admin/api/blocked_banks">
+                    <datatable
+                        ref="datatable"
+                        title="Blocked Banks"
+                        :rows="tableData"
+                        :columns="columndata"
+                        :columnDefs="columnDefs"
+                        @OpenModal="open_modal"
+                        url="admin/api/blocked_banks">
                     </datatable>
                 </div>
             </b-card>
         </div>
+        <b-modal id="modal1" ref="modal1" title="Delete bank" ok-title="Yes" cancel-title="No"
+                                                 ok-variant="danger" @ok="deleteItem">
+            <p class="my-4">Are you sure want to delete this bank?</p>
+        </b-modal>
     </div>
 </template>
 <script type="text/javascript">
     import datatable from "components/plugins/DataTable/DataTable.vue";
+    import ApiService from "resources/common/api.service";
+    import miniToastr from 'mini-toastr';
+    miniToastr.init();
 
     export default {
         name: "banks_list",
@@ -40,6 +54,18 @@
             }
         },
         methods: {
+            open_modal(id) {
+                this.bank_id = id;
+                this.$refs.modal1.show();
+            },
+            deleteItem() {
+                ApiService.delete('admin/api/banks/' + this.bank_id).then(response => {
+                    miniToastr.success("Bank has been deleted successfully", "Success")
+                    this.$refs.datatable.reload();
+                })
+                .catch(function (error) {
+                });
+            }
         },
         mounted() {
         },
